0eba9b4a6e266c3014563be60450005eb4b8e265
Adrien LUCAS <adrien.lucas@sensiolabs.com>
2022-02-08 15:39:37 +0100

    Add loadDemo route + list movies

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,19 +12,64 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'movie')]
-    #[Route('/', name: 'homepage')]
-    public function index(): Response
+    #[Route('/movie/{id}', name: 'movie')]
+    public function index(Movie $movie): Response
     {
-        $movie = [
-            'title' => 'The Return of the King',
-            'year' => 2003,
-            'director' => 'Peter Jackson',
-            'description' => 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
-        ];
-
         return $this->render('movie/index.html.twig', [
             'movie' => $movie,
         ]);
+    }
+    
+    #[Route('/list-movies', name: 'list_movie')]
+    #[Route('/', name: 'homepage')]
+    public function list(): Response
+    {
+        $entityManager = $this->container->get(EntityManagerInterface::class);
+        $movies = $entityManager->getRepository(Movie::class)->findAll();
+
+        return $this->render('movie/list.html.twig', [
+            'movies' => $movies,
+        ]);
+    }
+
+    #[Route('/loaddemo', name: 'loaddemo')]
+    public function loadDemo(): Response
+    {
+        /** @var Registry $doctrine */
+//        $doctrine = $this->container->get('doctrine');
+//        $entityManager = $doctrine->getManager();
+
+        $entityManager = $this->container->get(EntityManagerInterface::class);
+
+        // Create the first movie
+        $movie = new Movie();
+        $movie->setTitle('Don\'t look up');
+        $movie->setDirector('Adam McKay');
+        $movie->setYear(2021);
+        $movie->setDescription('Two low-level astronomers must go on a giant media tour to warn mankind of an approaching comet that will destroy planet Earth.');
+
+        $entityManager->persist($movie);
+
+        // Create the second movie
+        $movie = new Movie();
+        $movie->setTitle('The Blues Brothers');
+        $movie->setDirector('John Landis');
+        $movie->setYear(1980);
+        $movie->setDescription('Jake Blues rejoins with his brother Elwood after being released from prison, but the duo has just days to reunite their old R&B band and save the Catholic home where the two were raised, outrunning the police as they tear through Chicago.');
+
+        $entityManager->persist($movie);
+
+        $entityManager->flush();
+
+        return new Response('OK');
+    }
+
+    public static function getSubscribedServices(): array
+    {
+        $services = parent::getSubscribedServices();
+        $services[] = EntityManagerInterface::class;
+//        $services['doctrine'] = Registry::class;
+
+        return $services;
     }
 }
