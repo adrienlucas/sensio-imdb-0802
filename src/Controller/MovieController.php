67286805393891d514c188da8e246b3880c35896
Adrien LUCAS <adrien.lucas@sensiolabs.com>
2022-02-08 12:20:02 +0100

    Ajout structure Movie (et rendus)

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,14 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
     #[Route('/movie', name: 'movie')]
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-        return $this->render('movie/index.html.twig', []);
+        $movie = [
+            'title' => 'The Return of the King',
+            'year' => 2003,
+            'director' => 'Peter Jackson',
+            'description' => 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
+        ];
+
+        return $this->render('movie/index.html.twig', [
+            'movie' => $movie,
+        ]);
     }
 }
