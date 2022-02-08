97831f1fd904a71ec57dcac1f3046d90a7055886
Adrien LUCAS <adrien.lucas@sensiolabs.com>
2022-02-08 12:21:43 +0100

    Ajout de l'entité Movie (+ parametre DATABASE_URL)

---
+++ src/Entity/Movie.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $title;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $year;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $director;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $description;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(?int $year): void
+    {
+        $this->year = $year;
+    }
+
+    public function getDirector(): string
+    {
+        return $this->director;
+    }
+
+    public function setDirector(string $director): void
+    {
+        $this->director = $director;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+}
+
